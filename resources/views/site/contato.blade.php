29e13a027e47f2d1b33efc7e41705155bd28de2b
Dilceu Lopes <dilceulopesjr181@gmail.com>
2024-09-22 10:46:10 -0300

    [ADD]avançando no curso laravel

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,1 +1,14 @@
 <h3>Contato (view)</h3>
+
+<ul>
+    <li>
+        <a href="{{route('homepage')}}">Home</a>
+    </li>
+    <li>
+        <a href="{{route('sobrenos')}}">Sobre nós</a>
+    </li>
+    <li>
+        <a href="{{route('contato')}}">Contato</a>
+    </li>
+    
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,44 @@ Route::get('/', function () {
     return 'Olá, seja bem vindo ao curso!';
 });*/
 
-Route::get('/', 'PrincipalController@principal');
-
 /*
 Route::get('/sobre-nos', function () {
     return 'Sobre-nós';
 });*/
-
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 /*
 Route::get('/contato', function () {
     return 'Contato';
 });*/
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/', 'PrincipalController@principal')->name('homepage');
 
-//nome, categoria, assunto, mensagem
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('sobrenos');
+
+Route::get('/contato', 'ContatoController@contato')->name('contato');
+
+
+Route::prefix('/app')->name('app.')->group(function(){
+    Route::get('/login', function(){return 'Login'; })->name('login');
+    Route::get('/clientes', function(){return 'Clientes'; })->name('clientes');
+    Route::get('/fornecedores', function(){return 'Fornecedores'; })->name('fornecedores');
+    Route::get('/produtos', function(){return 'produtos'; })->name('produtos');
+});
+
+Route::get('/rota1', function(){
+    echo 'Rota 1';
+})->name('rota1');
+
+Route::get('/rota2', function(){
+    //echo 'Rota 2';
+    return redirect()->route('rota1');
+})->name('rota2');
+
+// Route::redirect('rota2', 'rota1');
+
+Route::fallback(function(){
+    echo 'Rota acessada não existe. Clique <a href="'.route('homepage').'">aqui</a> para acessar a homepage:';
+});
 
 Route::get('/contato/{nome}/{categoria_id}',
     function(
